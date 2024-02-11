d23415702e5ef0221155509a9a9e7968fcb5c60f
ishannjr <87802247+ishannjr@users.noreply.github.com>
2024-02-10 21:34:26 -0800

    Configured-normal-bugs

--- Assets/Scripts/GlobalVariabless.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GlobalVariables : MonoBehaviour
-{
-    public static bool isDeadP1 = false;
-    public static bool isDeadP2 = false;
-}
